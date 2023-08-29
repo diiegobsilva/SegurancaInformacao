52aadbb13f7bdcf0c978010fa527f14dd55d9a53
mateushlsilva <mateushls01@gmail.com>
2023-08-29 18:14:35 -0300

    Atualiza script

--- server/clientedel.sh
+++ server/clientedel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 BKPNUMERO=$(echo $(cat logs/deleteuser.log | grep -n 27 | cut -d ":" -f1 | cut -d " " -f1) | cut -d " " -f1)
+if [ $BKPNUMERO -eq 1 ]
+then
+    BKPNUMERO=0
+fi
 LINHAS=$( wc -l logs/deleteuser.log | cut -d " " -f1)
 NUMERO_LINHAS_ARQ=$(expr 1 + $LINHAS )
 NUMERO_LINHAS=$(expr $NUMERO_LINHAS_ARQ - $BKPNUMERO )
@@ -9,5 +13,5 @@ IDS=$(tail -n $NUMERO_LINHAS logs/deleteuser.log | cut -d ":" -f6)
 for i in $IDS    
 do
         echo "valor - $i"
-        docker exec mysql-server mysql -h"localhost" -u"root" -p"fatec" -e"use clientes;"  -e "DELETE FROM cliente WHERE id = 9;"
+        docker exec mysql-server mysql -h"localhost" -u"root" -p"fatec" -e"use clientes;"  -e "DELETE FROM cliente WHERE id = $i;"
 done

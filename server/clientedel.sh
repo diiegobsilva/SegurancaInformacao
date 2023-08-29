1a2a604cd7b8926a0bf368ca4965bc2325847512
mateushlsilva <mateushls01@gmail.com>
2023-08-29 17:16:50 -0300

    Atualiza script

--- server/clientedel.sh
+++ server/clientedel.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 BKPNUMERO=$(cat logs/deleteuser.log | grep -n 11 | cut -d ":" -f1)
-
 LINHAS=$( wc -l logs/deleteuser.log | cut -d " " -f1)
-NUMERO_LINHAS=$(expr 1 + $LINHAS )
+NUMERO_LINHAS_ARQ=$(expr 1 + $LINHAS )
+NUMERO_LINHAS=$(expr $NUMERO_LINHAS_ARQ - $BKPNUMERO )
+IDS=$(tail -n $NUMERO_LINHAS logs/deleteuser.log | cut -d ":" -f6)
 
-echo $NUMERO_LINHAS
+for i in $IDS    
+do
+        echo "valor - $i"
+done

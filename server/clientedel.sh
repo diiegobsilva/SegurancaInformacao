5b2202511beaf8d12cd2079191638a1da33c4bcc
mateushlsilva <mateushls01@gmail.com>
2023-09-03 10:42:24 -0300

    Atualiza script clientedel.sh

--- server/clientedel.sh
+++ server/clientedel.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-BKPNUMERO=$(echo $(cat logs/deleteuser.log | grep -n 27 | cut -d ":" -f1 | cut -d " " -f1) | cut -d " " -f1)
-if [ $BKPNUMERO -eq 1 ]
-then
-    BKPNUMERO=0
-fi
-LINHAS=$( wc -l logs/deleteuser.log | cut -d " " -f1)
-NUMERO_LINHAS_ARQ=$(expr 1 + $LINHAS )
-NUMERO_LINHAS=$(expr $NUMERO_LINHAS_ARQ - $BKPNUMERO )
-IDS=$(tail -n $NUMERO_LINHAS logs/deleteuser.log | cut -d ":" -f6)
+MONGO_JSON=$(docker exec MONGODB_APP mongosh --quiet -eval "use clientes" -eval "db.cliente.find({ date: { \$gte: ISODate('2023-09-03T00:00:00Z') } })")
+
+IDS=$(echo $MONGO_JSON | sed 's/{/{\n/g; s/}/\n}/g; s/,/\n,/g' | grep cli_id | cut -d ":" -f2 | sed 's/ //g' )
 
 for i in $IDS    
 do
-        echo "valor - $i"
-        docker exec mysql-server mysql -h"localhost" -u"root" -p"fatec" -e"use clientes;"  -e "DELETE FROM cliente WHERE id = $i;"
+        echo "Deletando usuario com ID: $i"
+        docker exec mysql-server mysql -h"localhost" -u"root" -p"fatec" -e"use clientes;"  -e "DELETE FROM cliente WHERE id = $i;" > /dev/null 2>&1
 done

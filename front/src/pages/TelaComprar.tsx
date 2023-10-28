a3e3839b6fd7508b802e65570f3251db11c5c7d9
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2023-10-28 09:50:36 -0300

    telacompra

--- front/src/pages/TelaComprar.tsx
+++ front/src/pages/TelaComprar.tsx
@@ -4,17 +4,6 @@ import pizza3 from "../images/pizza3.jpg";
 import pizza2 from "../images/pizza2.jpg";
 
 function TelaComprar() {
-
-
-
-  const calculateTotal = (pizzas: any) => {
-    let total = 0;
-    for (const pizza of pizzas) {
-      total += pizza.price;
-    }
-    return total;
-  };
-
   return (
     <form>
       <div className="text-center mb-4">
@@ -25,24 +14,24 @@ function TelaComprar() {
       <div className="d-flex">
         <div className="col-lg-4">
           <div className="menu-column">
-                <img src={pizza3} alt="Brócolis e Queijo Provolone" />
-                <p className="item-description"> Brócolis e Queijo Provolone</p>
-                <h5 className="item-name">Preço: R$ 35,00</h5>
+            <img src={pizza3} alt="Brócolis e Queijo Provolone" />
+            <p className="item-description"> Brócolis e Queijo Provolone</p>
+            <h5 className="item-name">Preço: R$ 35,00</h5>
           </div>
         </div>
         <div className="col-lg-4">
           <div className="menu-column">
-                <img src={pizza5} alt="Calabresa e Brócolis" />
-                <p className="item-description">Brócolis e Queijo Branco</p>
-                <h5 className="item-name">Preço: R$ 45,00</h5>
+            <img src={pizza5} alt="Calabresa e Brócolis" />
+            <p className="item-description">Brócolis e Queijo Branco</p>
+            <h5 className="item-name">Preço: R$ 45,00</h5>
           </div>
         </div>
 
         <div className="col-lg-4">
           <div className="menu-column">
-                <img src={pizza2} alt="Calabresa, Cebola e Tomates" />
-                <p className="item-description">Calabresa, Cebola e Tomate</p>
-                <h5 className="item-name">Preço: R$ 45,00</h5>
+            <img src={pizza2} alt="Calabresa, Cebola e Tomates" />
+            <p className="item-description">Calabresa, Cebola e Tomate</p>
+            <h5 className="item-name">Preço: R$ 45,00</h5>
           </div>
         </div>
       </div>
@@ -52,27 +41,11 @@ function TelaComprar() {
           <label>Total:</label>
           <input type="text" value={'R$ 125,00'} readOnly />
         </div>
-        <div className="col-lg-6 forma-pagamento-input">
 
-          <div className="checkbox-container">
-          <div className="form-pagamento-label">
-            <label className="form-label">Forma de Pagamento:</label>
-          </div>
-            <label>
-              <input type="checkbox" /> Cartão
-            </label>
-            <label>
-              <input type="checkbox" /> Pix
-            </label>
-            <label>
-              <input type="checkbox" /> Dinheiro
-            </label>
-          </div>
-        </div>
-        </div>
+      </div>
       <div className="button-container d-flex justify-content-center mt-4">
         <button type="button" className="buttonFinalizar" >Finalizar Compra</button>
-     </div>
+      </div>
     </form>
   );
 }

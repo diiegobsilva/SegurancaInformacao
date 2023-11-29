ad5ea0d1d35d7d5948ce0884389727626f485263
mateushlsilva <mateushls01@gmail.com>
2023-11-29 07:29:13 -0300

    Atualiza

--- seguranca/func/__init__.py
+++ seguranca/func/__init__.py
@@ -1,8 +1,7 @@
 import os
 import shlex
 import subprocess
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import PathCompleter
+
 
 def listar_arquivos(pasta):
     comando = f'ls {pasta}'
